b705c13efaa5af1b57925d8eb34d7cce6b3bc986
HC1058503505 <1058503505@qq.com>
2019-12-06 19:09:48 +0800

    完善诗词详情页

--- src/request/request.js
+++ src/request/request.js
@@ -5,6 +5,11 @@ export default function (params) {
     if (!params.baseUrl) {
         params.baseUrl = baseUrl
     } 
+    
+    if (!params.data.token) {
+        params.data.token = 'gswapi'
+    }
+
     params.url = params.baseUrl + params.url
     return wepy.request(params)
 }
